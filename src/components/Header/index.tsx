b42bfa0dd5394cf2c7c6fc4bdc63fda433ed1675
Joao Victor Santos <joaovictorsantoscpv@gmail.com>
2020-07-21 15:03:32 -0300

    add: create structure without stylize header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
 
-const Header: React.FC = () => <h1>teste</h1>;
+import { GoMarkGithub, GoTriangleDown } from 'react-icons/go';
+import { BsBell } from 'react-icons/bs';
+import { AiOutlinePlus } from 'react-icons/ai';
+
+import { Container } from './styles';
+
+const Header: React.FC = () => (
+  <Container>
+    <div>
+      <GoMarkGithub />
+      <input />
+      <ul>
+        <li>Pull requests</li>
+        <li>Issues</li>
+        <li>Marketpalce</li>
+        <li>Explore</li>
+      </ul>
+    </div>
+    <div>
+      <BsBell />
+      <AiOutlinePlus />
+      <GoTriangleDown />
+      <img
+        src="https://avatars2.githubusercontent.com/u/31141391?s=60&v=4"
+        alt="Profile avatar"
+      />
+      <GoTriangleDown />
+    </div>
+  </Container>
+);
 
 export default Header;
