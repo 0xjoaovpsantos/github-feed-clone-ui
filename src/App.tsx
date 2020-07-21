5ebd5adf4bfb6fe70c2492f5dc3d020e00245bab
Joao Victor Santos <joaovictorsantoscpv@gmail.com>
2020-07-21 14:23:30 -0300

    add: style global

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import React from "react";
+import React from 'react';
+
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Header></Header>;
 }
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header: React.FC = () => <h1>teste</h1>;
+
+export default Header;
